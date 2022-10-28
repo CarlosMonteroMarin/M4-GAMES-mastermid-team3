a18e58fa1ef20165bc3621ab56414828ecacaa35
Isaac0312 <isaacejarque@hotmail.com>
2022-10-28 10:05:28 +0200

    Push variables básicas

--- M4-GAMES/src/main/java/M4_GAMES/M4_GAMES/Panel_Base.java
+++ M4-GAMES/src/main/java/M4_GAMES/M4_GAMES/Panel_Base.java
@@ -1,13 +1,24 @@
 package M4_GAMES.M4_GAMES;
 
-import java.awt.EventQueue;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTable;
 
 public class Panel_Base extends JFrame {
-
+	
+	private Color colors;
+	private Color colores_disponibles;
+	private int dificultad;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
@@ -19,11 +30,21 @@ public class Panel_Base extends JFrame {
 	 */
 	public Panel_Base() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 512, 512);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Colores Disponibles");
+		lblNewLabel.setBounds(321, 36, 124, 14);
+		contentPane.add(lblNewLabel);
 	}
-
+	
+	
+	
+	public void ColoresDisponibles() {
+		colors.
+	}
 }
