1cf54d95ea5e547b76ea0ef6827e5488f8d1e81f
Carlos <CarlosMonteroMarin@carlosmonteromarin@gmail.com>
2022-10-28 09:19:13 +0200

    Proyecto base

---
+++ M4-GAMES/src/main/java/M4_GAMES/M4_GAMES/App.java
@@ -0,0 +1,13 @@
+package M4_GAMES.M4_GAMES;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ M4-GAMES/src/test/java/M4_GAMES/M4_GAMES/AppTest.java
@@ -0,0 +1,20 @@
+package M4_GAMES.M4_GAMES;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
